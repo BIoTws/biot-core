a3c658dd35bcfe171bcf9e198bafb11f7ceba1b3
Evgenii <xxjenekxx@gmail.com>
2019-10-09 16:22:13 +0300

    fixed sql

--- sql/create_sqlite_tables.js
+++ sql/create_sqlite_tables.js
@@ -2,36 +2,36 @@
 "use strict";
 const libToES6 = require('../lib/toEs6');
 
-async function create() {
-	await libToES6.dbQuery("CREATE TABLE IF NOT EXISTS aa_channels (  \\n\\\
-    aa_address CHAR(32) NOT NULL PRIMARY KEY, \\n\\\
-    salt VARCHAR(50) DEFAULT NULL,\\n\\\
-    definition TEXT,\\n\\\
-    asset CHAR(44) DEFAULT NULL,\\n\\\
-    peer_address CHAR(32) NOT NULL, \\n\\\
-    peer_device_address CHAR(33) DEFAULT NULL, \\n\\\
-    peer_url VARCHAR(100) DEFAULT NULL,\\n\\\
-    is_known_by_peer TINYINT DEFAULT 0,\\n\\\
-    is_peer_ready TINYINT DEFAULT 0,\\n\\\
-    amount_spent_by_peer INTEGER DEFAULT 0,\\n\\\
-    amount_spent_by_me INTEGER DEFAULT 0,\\n\\\
-    amount_deposited_by_peer INTEGER DEFAULT 0,\\n\\\
-    amount_deposited_by_me INTEGER DEFAULT 0,\\n\\\
-    amount_possibly_lost_by_me INTEGER DEFAULT 0,\\n\\\
-    overpayment_from_peer INTEGER DEFAULT 0,\\n\\\
-    auto_refill_threshold INTEGER DEFAULT 0,\\n\\\
-    auto_refill_amount INTEGER DEFAULT 0,\\n\\\
-    close_timestamp INTEGER,\\n\\\
-    timeout INTEGER NOT NULL,\\n\\\
-    period INTEGER DEFAULT 1,\\n\\\
-    last_message_from_peer TEXT,\\n\\\
-    last_event_id INTEGER DEFAULT 0,\\n\\\
-    is_definition_confirmed TINYINT DEFAULT 0,\\n\\\
-    closing_authored TINYINT DEFAULT 0,\\n\\\
-    status VARCHAR(30) DEFAULT 'created',\\n\\\
-    last_updated_mci INTEGER DEFAULT 0,\\n\\\
-    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\\n\\\
-    UNIQUE (peer_address, salt)\n\
+async function create(){
+	await libToES6.dbQuery("CREATE TABLE IF NOT EXISTS aa_channels (  \n\
+	aa_address CHAR(32) NOT NULL PRIMARY KEY, \n\
+	salt VARCHAR(50) DEFAULT NULL,\n\
+	definition TEXT,\n\
+	asset CHAR(44) DEFAULT NULL,\n\
+	peer_address CHAR(32) NOT NULL, \n\
+	peer_device_address CHAR(33) DEFAULT NULL, \n\
+	peer_url VARCHAR(100) DEFAULT NULL,\n\
+	is_known_by_peer TINYINT DEFAULT 0,\n\
+	is_peer_ready TINYINT DEFAULT 0,\n\
+	amount_spent_by_peer INTEGER DEFAULT 0,\n\
+	amount_spent_by_me INTEGER DEFAULT 0,\n\
+	amount_deposited_by_peer INTEGER DEFAULT 0,\n\
+	amount_deposited_by_me INTEGER DEFAULT 0,\n\
+	amount_possibly_lost_by_me INTEGER DEFAULT 0,\n\
+	overpayment_from_peer INTEGER DEFAULT 0,\n\
+	auto_refill_threshold INTEGER DEFAULT 0,\n\
+	auto_refill_amount INTEGER DEFAULT 0,\n\
+	close_timestamp INTEGER,\n\
+	timeout INTEGER NOT NULL,\n\
+	period INTEGER DEFAULT 1,\n\
+	last_message_from_peer TEXT,\n\
+	last_event_id INTEGER DEFAULT 0,\n\
+	is_definition_confirmed TINYINT DEFAULT 0,\n\
+	closing_authored TINYINT DEFAULT 0,\n\
+	status VARCHAR(30) DEFAULT 'created',\n\
+	last_updated_mci INTEGER DEFAULT 0,\n\
+	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\n\
+	UNIQUE (peer_address, salt)\n\
 );");
 	await libToES6.dbQuery("CREATE TABLE IF NOT EXISTS aa_my_deposits (\n\
 	aa_address CHAR(32) NOT NULL, \n\
